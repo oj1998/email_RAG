3c5994d9b0595ef8af272934d2b658770a3f47fa
oj1998 <oliverwjimenez@gmail.com>
2025-02-18 15:58:19 -0600

    Update __init__.py

--- loaders/__init__.py
+++ loaders/__init__.py
@@ -1,1 +1,5 @@
+from .email_loader import EmailLoader
+from .simple_pipeline import SimpleEmailPipeline
+from .vector_store_loader import VectorStoreFactory
 
+__all__ = ['EmailLoader', 'SimpleEmailPipeline', 'VectorStoreFactory']
