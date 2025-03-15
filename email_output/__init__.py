8c73aa91d806213de7d0509353757ad36bfd6749
oj1998 <oliverwjimenez@gmail.com>
2025-03-15 16:09:15 -0500

    Create __init__.py

---
+++ email_output/__init__.py
@@ -0,0 +1,20 @@
+"""
+Email Output package for intent-based response formatting.
+
+This package provides components for detecting email query intents
+and formatting responses appropriately for each intent type.
+"""
+
+from .email_intent import EmailIntent, EmailIntentDetector, EmailIntentAnalysis
+from .email_formatter import FormatStyle, EmailFormatter, EmailResponse, EmailSource
+from .email_output_adapter import EmailOutputManager, enhanced_process_email_query
+
+__all__ = [
+    'EmailIntent',
+    'EmailIntentDetector',
+    'EmailIntentAnalysis',
+    'FormatStyle',
+    'EmailFormatter',
+    'EmailResponse',
+    'EmailSource',
+    'Email
