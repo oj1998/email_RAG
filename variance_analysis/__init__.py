eb0874201afee47376ef1dfa0d67076b3ce66407
oj1998 <oliverwjimenez@gmail.com>
2025-03-23 19:46:27 -0500

    Update __init__.py

--- variance_analysis/__init__.py
+++ variance_analysis/__init__.py
@@ -1,1 +1,21 @@
+"""
+Document Variance Analysis Module
 
+This package provides tools for analyzing variances between multiple documents
+on the same topic, helping users identify inconsistencies in source material.
+"""
+
+from .document_variance_analyzer import DocumentVarianceAnalyzer, DocumentVariance, TopicVarianceAnalysis
+from .variance_formatter import VarianceFormatter, VarianceFormatStyle
+from .variance_retriever import VarianceDocumentRetriever
+
+__version__ = "0.1.0"
+
+__all__ = [
+    'DocumentVarianceAnalyzer',
+    'DocumentVariance',
+    'TopicVarianceAnalysis',
+    'VarianceFormatter',
+    'VarianceFormatStyle',
+    'VarianceDocumentRetriever'
+]
